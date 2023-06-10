5bf698510c284e06679ca1ddda6288d8b530ab1f
Zaid Ahmad <zaidd250@gmail.com>
2023-06-10 20:37:47 +0530

    remove models, controllers

--- app.js
+++ app.js
@@ -9,16 +9,6 @@ const newMsgRouter = require("./routes/new")
 
 const app = express()
 
-const mongoose = require("mongoose")
-mongoose.set("strictQuery", false)
-const mongoDB =
-  "mongodb+srv://admin:admin@cluster0.femq49x.mongodb.net/?retryWrites=true&w=majority"
-
-main().catch((err) => console.log(err))
-async function main() {
-  await mongoose.connect(mongoDB)
-}
-
 // view engine setup
 app.set("views", path.join(__dirname, "views"))
 app.set("view engine", "jade")
