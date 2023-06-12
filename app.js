a1678e392390b2b3f6e7dc1c3933e984bc4c4935
Zaid Ahmad <zaidd250@gmail.com>
2023-06-12 21:27:33 +0530

    made changes for deployment

--- app.js
+++ app.js
@@ -3,10 +3,24 @@ const express = require("express")
 const path = require("path")
 const cookieParser = require("cookie-parser")
 const logger = require("morgan")
+const compression = require("compression")
+const helmet = require("helmet")
 
 const indexRouter = require("./routes/index")
 
 const app = express()
+
+const RateLimit = require("express-rate-limit")
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 20,
+})
+// Apply rate limiter to all requests
+app.use(limiter)
+
+app.use(helmet())
+
+app.use(compression()) // Compress all routes
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"))
